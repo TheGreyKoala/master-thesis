514b65b23c83522c6aee3a09a9469fbba5cfaa0b
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-10-29 19:17:05 +0100

    First mindmaps, chapters, abstract

---
+++ Mindmaps/Problemanalyse.mm
@@ -0,0 +1,85 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Problemanalyse" FOLDED="false" ID="ID_1345478669" CREATED="1509291239202" MODIFIED="1509291244879" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Wordpress" POSITION="right" ID="ID_751869173" CREATED="1509291248516" MODIFIED="1509291251750">
+<edge COLOR="#ff0000"/>
+<node TEXT="Plugins" ID="ID_1573489126" CREATED="1509291253483" MODIFIED="1509291283250">
+<node TEXT="Speichern nicht HTML, sondern Markup in DB" ID="ID_952637016" CREATED="1509291285010" MODIFIED="1509291317477"/>
+<node TEXT="Werden ggf. in Template eingebunden &amp; speichern keine Daten in DB" ID="ID_97099715" CREATED="1509291426547" MODIFIED="1509291470137">
+<node TEXT="Z. B. Veranstaltungsalendar" ID="ID_1909808443" CREATED="1509291480040" MODIFIED="1509291519771"/>
+</node>
+</node>
+<node TEXT="M&#xf6;glichkeiten der Abfrage der Daten" ID="ID_1371482794" CREATED="1509291327233" MODIFIED="1509291338969">
+<node TEXT="Datenbank" ID="ID_779349682" CREATED="1509291341441" MODIFIED="1509291348057"/>
+<node TEXT="REST" ID="ID_1317442507" CREATED="1509291348436" MODIFIED="1509291351893"/>
+<node TEXT="HTML" ID="ID_257469261" CREATED="1509291352079" MODIFIED="1509291353962"/>
+</node>
+</node>
+<node TEXT="Inhalte haben hierarchische Struktur" POSITION="right" ID="ID_571061281" CREATED="1509291992382" MODIFIED="1509291999144">
+<edge COLOR="#0000ff"/>
+</node>
+</node>
+</map>
---
+++ Mindmaps/Relaunch-an-der-FernUni.mm
@@ -0,0 +1,134 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Relaunch an der FernUni" FOLDED="false" ID="ID_941076615" CREATED="1509274613165" MODIFIED="1509274626810" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="20" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Webauftritt der FernUni schon alt" POSITION="right" ID="ID_1795204605" CREATED="1509274803753" MODIFIED="1509276987602">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Neues Responsive Design" POSITION="right" ID="ID_1521864993" CREATED="1509274815893" MODIFIED="1509274825853">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="Inhalte werden &#xfc;bernommen" POSITION="right" ID="ID_713822935" CREATED="1509274826258" MODIFIED="1509274832468">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="Imperia ist gesetztes CMS" POSITION="right" ID="ID_1590421889" CREATED="1498928055173" MODIFIED="1509274786449">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Die meisten Seiten basieren bereits auf Imperia" POSITION="right" ID="ID_289284198" CREATED="1509274863366" MODIFIED="1509274882424">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Migration ist einfache Modell-Transformation" ID="ID_270502072" CREATED="1509274888234" MODIFIED="1509274930383"/>
+<node TEXT="Bereits in Gange, mit passenden Skripten f&#xfc;r jede Seite" ID="ID_987006961" CREATED="1509274930835" MODIFIED="1509274955948"/>
+</node>
+<node TEXT="KSW setzt auf Wordpress" POSITION="right" ID="ID_1489828301" CREATED="1498928080099" MODIFIED="1509274974156">
+<edge COLOR="#00ff00"/>
+<node TEXT="Eigenes Design, deshalb wirkt Auftritt insgesamt nicht wie aus &quot;einem Guss&quot;" ID="ID_366787627" CREATED="1509274992788" MODIFIED="1509275100759"/>
+<node TEXT="Separate Datenhaltung" ID="ID_945108476" CREATED="1509274998452" MODIFIED="1509275009779"/>
+<node TEXT="Erh&#xf6;hter Administrationsaufwand" ID="ID_1306368566" CREATED="1509275034709" MODIFIED="1509275040384"/>
+<node TEXT="Verschiedene Systeme f&#xfc;r Readkteure" ID="ID_1889728742" CREATED="1509275023323" MODIFIED="1509275034477"/>
+</node>
+<node TEXT="Migration der KSW-Seiten zu Imperia" POSITION="right" ID="ID_1919899119" CREATED="1498928092914" MODIFIED="1509274786455">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Inhalte sollen auch hier &#xfc;bernommen werden" ID="ID_1143890186" CREATED="1509275133910" MODIFIED="1509275160411"/>
+<node TEXT="Alte/&#xfc;berfl&#xfc;ssige Inhalte ggf. aussortieren" ID="ID_377560598" CREATED="1509275160688" MODIFIED="1509275178993"/>
+</node>
+<node TEXT="WP-Inhalte sind unstrukturiert" POSITION="right" ID="ID_419543377" CREATED="1507654561173" MODIFIED="1509274786459">
+<edge COLOR="#00ffff"/>
+<node TEXT="Titel" ID="ID_1904032399" CREATED="1509275216527" MODIFIED="1509275220114"/>
+<node TEXT="Inhalt" ID="ID_1703151174" CREATED="1509275220306" MODIFIED="1509275224077"/>
+<node TEXT="https://codex.wordpress.org/Database_Description#Table:_wp_posts" ID="ID_1110656849" CREATED="1509275233809" MODIFIED="1509275329256"/>
+</node>
+<node TEXT="Idee: Migration in zwei Schritten" POSITION="right" ID="ID_1896343385" CREATED="1509275357454" MODIFIED="1509275775100">
+<edge COLOR="#ff0000"/>
+<node TEXT="WP-Inhalte automatisiert strukturieren" ID="ID_754069238" CREATED="1509275363546" MODIFIED="1509275757365"/>
+<node TEXT="Migrationstool macht Modell-Transformation" ID="ID_43276041" CREATED="1509275692008" MODIFIED="1509275732519"/>
+</node>
+<node TEXT="Webauftritt f&#xfc;r alle Unternehmen etc. wichtig" POSITION="left" ID="ID_638668301" CREATED="1509276007131" MODIFIED="1509276024425">
+<edge COLOR="#0000ff"/>
+<node TEXT="Marketing f&#xfc;r sich selbst" ID="ID_604062958" CREATED="1509276027610" MODIFIED="1509276034008"/>
+<node TEXT="Bereitstellung von Informationen" ID="ID_209620980" CREATED="1509276034223" MODIFIED="1509276041866"/>
+<node TEXT="Gute &amp; aktuelle Inhalte entscheidend f&#xfc;r Erfolg" ID="ID_724978565" CREATED="1509276378117" MODIFIED="1509276395307"/>
+</node>
+<node TEXT="Pflege der Inhalte &#xfc;bernehmen Redakteure" POSITION="left" ID="ID_448407350" CREATED="1509276446394" MODIFIED="1509276457840">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Pflege der Inhalte geschieht &#xfc;ber CMS" POSITION="left" ID="ID_1109516023" CREATED="1509276397750" MODIFIED="1509276409770">
+<edge COLOR="#00ffff"/>
+<node TEXT="Versteckt Technik hinter der Webseite" ID="ID_1491846768" CREATED="1509276476074" MODIFIED="1509276493426"/>
+<node TEXT="Nutzerfreundliche Pflege" ID="ID_1703621471" CREATED="1509276493938" MODIFIED="1509276504910"/>
+<node TEXT="Speicherung der Inhalte" ID="ID_1960108969" CREATED="1509276505172" MODIFIED="1509276511354"/>
+</node>
+<node TEXT="State Of The Art f&#xfc;r Struktur &amp; Design &#xe4;ndert sich laufen" POSITION="left" ID="ID_984054342" CREATED="1509276064723" MODIFIED="1509276133141">
+<edge COLOR="#00ff00"/>
+<node TEXT="Bsp. Responsive Design" ID="ID_1924077447" CREATED="1509276824060" MODIFIED="1509276829834"/>
+</node>
+<node TEXT="Webseiten m&#xfc;ssen regelm&#xe4;&#xdf;ig erneuert werden" POSITION="left" ID="ID_1991036389" CREATED="1509276832539" MODIFIED="1509276850192">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Inhalte m&#xfc;ssen migriert werden" POSITION="left" ID="ID_1974923409" CREATED="1509276874736" MODIFIED="1509276910933">
+<edge COLOR="#7c007c"/>
+<node TEXT="Mal mehr, mal weniger" ID="ID_314789290" CREATED="1509276967929" MODIFIED="1509276971864"/>
+</node>
+</node>
+</map>
