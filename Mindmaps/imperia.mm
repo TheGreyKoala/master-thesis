ee2124119d2bb67c20fb880deb14ec51682e54d9
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-11-11 12:49:38 +0100

    imperia CMS preparation

---
+++ Mindmaps/imperia.mm
@@ -0,0 +1,194 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="imperia CMS" FOLDED="false" ID="ID_368301264" CREATED="1510396058737" MODIFIED="1510396064813" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="[Kapitel 1.1.2] Dokumente" POSITION="right" ID="ID_1257614058" CREATED="1510396879198" MODIFIED="1510396936799">
+<edge COLOR="#0000ff"/>
+<node TEXT="[Kapitel 1.1]" ID="ID_1024610911" CREATED="1510399046494" MODIFIED="1510399052718">
+<node TEXT="Zur Pflege und Speicherung von Inhalt" ID="ID_531380251" CREATED="1510399448334" MODIFIED="1510399459192"/>
+<node TEXT="K&#xf6;nnen vom Nutzer mit Inhalt gef&#xfc;llt werden" ID="ID_1126226907" CREATED="1510396603259" MODIFIED="1510396614172"/>
+<node TEXT="Werden in Kategorien eingeteilt (Hierarchische Strukturierung)" ID="ID_1351652634" CREATED="1510396415637" MODIFIED="1510399497568"/>
+</node>
+<node TEXT="Sammlung von Key-Value-Paaren" ID="ID_682877047" CREATED="1510396891810" MODIFIED="1510396898599"/>
+<node TEXT="Keys" ID="ID_1810865186" CREATED="1510397001199" MODIFIED="1510397003384">
+<node TEXT="Name eines Eingabefeldes" ID="ID_1582949293" CREATED="1510396939974" MODIFIED="1510397009928"/>
+<node TEXT="Auch Metavariablen genannt" ID="ID_1177459909" CREATED="1510397012847" MODIFIED="1510397020224"/>
+</node>
+<node TEXT="Value ist der Inhalt dieses Feldes" ID="ID_233440504" CREATED="1510396961471" MODIFIED="1510396967695"/>
+<node TEXT="Layout-unabh&#xe4;ngig" ID="ID_713139671" CREATED="1510399557203" MODIFIED="1510399568268"/>
+<node TEXT="Inhalt k&#xf6;nnen einfach in verschiedene Formate &#xfc;bertragen werden" ID="ID_1774574018" CREATED="1510397069200" MODIFIED="1510397084745"/>
+</node>
+<node TEXT="[Kapitel 1.1.4] Vorlagen" POSITION="right" ID="ID_1744379707" CREATED="1510397103840" MODIFIED="1510397117945">
+<edge COLOR="#00ff00"/>
+<node TEXT="Trennung von Layout und Inhalt" ID="ID_1638164479" CREATED="1510397249154" MODIFIED="1510397255755">
+<node TEXT="Dokumente speichern den Inhalt" ID="ID_612509182" CREATED="1510399726519" MODIFIED="1510399734685"/>
+<node TEXT="[Kapitel 1.1] Templates bestimmen das Layout" ID="ID_977170576" CREATED="1510399734868" MODIFIED="1510399847755"/>
+<node TEXT="Viele Dokumente k&#xf6;nnen auf demgleichen Template basieren" ID="ID_723811946" CREATED="1510399743852" MODIFIED="1510399757028"/>
+<node TEXT="[Kapitel 1.1]" ID="ID_1373826965" CREATED="1510399079790" MODIFIED="1510399086903">
+<node TEXT="Jeder Kategorie ist ein Template zugewiesen" ID="ID_475346470" CREATED="1510399088928" MODIFIED="1510399098607"/>
+</node>
+<node TEXT="[Kapitel 36]" ID="ID_1283073906" CREATED="1510399898104" MODIFIED="1510399903752">
+<node TEXT="Einfache Inhaltspflege ohne technische Kentnisse erm&#xf6;glichen" ID="ID_122552910" CREATED="1510398451113" MODIFIED="1510399947937"/>
+<node TEXT="Braucht Eingabemasken / Formulare" ID="ID_50786291" CREATED="1510398496511" MODIFIED="1510399977848"/>
+<node TEXT="Inhalt muss in Layout integriert werden" ID="ID_855095529" CREATED="1510398506391" MODIFIED="1510398515192"/>
+</node>
+</node>
+<node TEXT="Eingabeformulare" ID="ID_221895821" CREATED="1510397127979" MODIFIED="1510397132682">
+<node TEXT="Normale Eingabekomponenten" ID="ID_988864118" CREATED="1510397180985" MODIFIED="1510397191650">
+<node TEXT="Textfelder" ID="ID_1660158370" CREATED="1510397192133" MODIFIED="1510397194746"/>
+<node TEXT="Checkboxen" ID="ID_1978762201" CREATED="1510397194897" MODIFIED="1510397199466"/>
+<node TEXT="etc." ID="ID_518497244" CREATED="1510397199641" MODIFIED="1510397200578"/>
+</node>
+<node TEXT="Spezielle Komponenten" ID="ID_1277353554" CREATED="1510397206737" MODIFIED="1510397213778">
+<node TEXT="flex modules" ID="ID_1711144353" CREATED="1510397214251" MODIFIED="1510397223066"/>
+<node TEXT="imperia modules" ID="ID_830938320" CREATED="1510397223329" MODIFIED="1510397232186"/>
+<node TEXT="word modules" ID="ID_338254109" CREATED="1510397232458" MODIFIED="1510397237387"/>
+</node>
+</node>
+<node TEXT="[Kapitel 36]" ID="ID_1961605096" CREATED="1510398445367" MODIFIED="1510398450304">
+<node TEXT="HTML-Datei mit" ID="ID_1853320718" CREATED="1510398767278" MODIFIED="1510398776367">
+<node TEXT="Input-Felder f&#xfc;r Eingabemake" ID="ID_535073257" CREATED="1510398777377" MODIFIED="1510398789215"/>
+<node TEXT="Ger&#xfc;st f&#xfc;r finale Seite" ID="ID_1706587013" CREATED="1510398789493" MODIFIED="1510398807695">
+<node TEXT="Referenzierung der Metavariablen des Dokumentes" ID="ID_1043207830" CREATED="1510400076421" MODIFIED="1510400087780"/>
+</node>
+</node>
+<node TEXT="2 Varianten" ID="ID_343852862" CREATED="1510398808966" MODIFIED="1510398812015">
+<node TEXT="Input-Felder &amp; Ger&#xfc;st in einer Datei" ID="ID_255217663" CREATED="1510398813415" MODIFIED="1510398825263">
+<node TEXT="WYSIWYG" ID="ID_1943245198" CREATED="1510398826223" MODIFIED="1510398858007"/>
+<node TEXT="Finale Seite enth&#xe4;lt keine Eingabefelder, sondern nur deren Inhalt" ID="ID_688381961" CREATED="1510398895502" MODIFIED="1510398914007"/>
+</node>
+<node TEXT="Input-Felder &amp; Ger&#xfc;st in getrennten Dateien" ID="ID_742132435" CREATED="1510398875270" MODIFIED="1510398887535">
+<node TEXT="Gut, wenn Inhalt in verschiedenen Layouts genutzt werden soll" ID="ID_1845602030" CREATED="1510398955110" MODIFIED="1510398970575"/>
+<node TEXT="Layout-neutrale Eingabemaske" ID="ID_774786843" CREATED="1510398937959" MODIFIED="1510398947183"/>
+<node TEXT="Verschiedene Templates f&#xfc;r verschiedene Layouts" ID="ID_690615495" CREATED="1510398974406" MODIFIED="1510398986375"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="[Kapitel 1.1.5] Workflows" POSITION="right" ID="ID_133142158" CREATED="1510397289930" MODIFIED="1510397298283">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Reihe von Verarbeitungsschritten" ID="ID_1535736015" CREATED="1510397777133" MODIFIED="1510397784684"/>
+<node TEXT="[Kapitel 1.1]" ID="ID_1120772450" CREATED="1510400306608" MODIFIED="1510400311833">
+<node TEXT="Bestimmt Reihenfolge der Schritte" ID="ID_36128524" CREATED="1510396673156" MODIFIED="1510396680117"/>
+</node>
+<node TEXT="Schritte m&#xfc;ssen von Dokument durchlaufen werden, bevor Publizierung" ID="ID_1900977209" CREATED="1510397794498" MODIFIED="1510397811122"/>
+<node TEXT="F&#xfc;r jede Kategorie definierbar" ID="ID_442939014" CREATED="1510397819481" MODIFIED="1510397827018"/>
+<node TEXT="[Kapitel 1.1]" ID="ID_1330269055" CREATED="1510399148446" MODIFIED="1510399157207">
+<node TEXT="Bsp." ID="ID_560983533" CREATED="1510396712476" MODIFIED="1510396715893">
+<node TEXT="Erstellung eines Dokumentens" ID="ID_117522621" CREATED="1510396716400" MODIFIED="1510396723717"/>
+<node TEXT="Pflege von Inhalten" ID="ID_1086857063" CREATED="1510396724028" MODIFIED="1510396728853"/>
+<node TEXT="Pr&#xfc;fung der Inhalte durch anderen Nutzer" ID="ID_922516682" CREATED="1510396729076" MODIFIED="1510396747646"/>
+<node TEXT="Publizierung" ID="ID_1928748744" CREATED="1510396747828" MODIFIED="1510396751653"/>
+</node>
+</node>
+</node>
+<node TEXT="[Kapitel 1.1] Architektur" POSITION="right" ID="ID_916929437" CREATED="1510396079220" MODIFIED="1510400599758">
+<edge COLOR="#ff0000"/>
+<node TEXT="Imperia Server" ID="ID_414890840" CREATED="1510396163412" MODIFIED="1510399272095">
+<node ID="ID_1294810721" CREATED="1510399278241" MODIFIED="1510399278241"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Entwicklungssystem
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="Anlegen &amp; Editieren von Dokumenten" ID="ID_1160060085" CREATED="1510396194636" MODIFIED="1510396203811"/>
+<node TEXT="Projekt strukturieren" ID="ID_311746654" CREATED="1510396337442" MODIFIED="1510396343283"/>
+<node TEXT="Inhalte zum Projekt hinzuf&#xfc;gen" ID="ID_132576716" CREATED="1510396343434" MODIFIED="1510396357868"/>
+<node TEXT="Ausf&#xfc;hrung von Workflows" ID="ID_163681186" CREATED="1510400677580" MODIFIED="1510400685397"/>
+<node TEXT="Datenhaltung" ID="ID_1735053507" CREATED="1510400715939" MODIFIED="1510400719908"/>
+</node>
+<node TEXT="Clients nutzen Weboberfl&#xe4;che" ID="ID_1117914990" CREATED="1510400633436" MODIFIED="1510400641997">
+<node TEXT="&#xdc;ber HTTP(S)" ID="ID_930872781" CREATED="1510400657286" MODIFIED="1510400664933"/>
+</node>
+<node TEXT="Zielsysteme" ID="ID_1444198516" CREATED="1510396171754" MODIFIED="1510396175395">
+<node TEXT="Z. B. ein Webserver" ID="ID_1019707216" CREATED="1510396500149" MODIFIED="1510396511835"/>
+<node TEXT="Unabh&#xe4;ngig vom Imperia Server" ID="ID_9332834" CREATED="1510400743955" MODIFIED="1510400754364"/>
+<node TEXT="Stellt Endkunden die Inhalte bereit" ID="ID_922485979" CREATED="1510400756467" MODIFIED="1510400775644"/>
+</node>
+<node TEXT="Publishing" ID="ID_688373656" CREATED="1510396226241" MODIFIED="1510396230778">
+<node TEXT="Sobald alle notwendigen Schritte f&#xfc;r Dokument fertig" ID="ID_106618634" CREATED="1510396449306" MODIFIED="1510396459291"/>
+<node TEXT="Dokumente werden generiert (statische Dokumente)" ID="ID_145518642" CREATED="1510400539237" MODIFIED="1510400578550"/>
+<node TEXT="Fertige Dokumente werden auf Zielsystem &#xfc;bertragen" ID="ID_396141752" CREATED="1510396231292" MODIFIED="1510396244387">
+<node TEXT="&#xdc;ber (S)FTP, WebDAV" ID="ID_84186448" CREATED="1510400781708" MODIFIED="1510400793252"/>
+</node>
+<node TEXT="Automatisch oder manuell" ID="ID_581905238" CREATED="1510396476202" MODIFIED="1510396481395"/>
+</node>
+</node>
+<node TEXT="Seit 1995" POSITION="left" ID="ID_1496970834" CREATED="1510401032315" MODIFIED="1510401062562">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Zun&#xe4;chst imperia AG, jetzt pirobase imperia GmbH" POSITION="left" ID="ID_102810159" CREATED="1510401063065" MODIFIED="1510401088498">
+<edge COLOR="#00007c"/>
+</node>
+</node>
+</map>
