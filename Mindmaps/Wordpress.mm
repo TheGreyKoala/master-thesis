4f1530298c925d32e64d8575ae8ca47f3d99959a
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-11-11 15:44:27 +0100

    wordpress preparation

---
+++ Mindmaps/Wordpress.mm
@@ -0,0 +1,204 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Wordpress" FOLDED="false" ID="ID_371452716" CREATED="1510401213297" MODIFIED="1510401217489" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.001">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Vorlagen &amp; Themes" POSITION="right" ID="ID_1208180572" CREATED="1510401964497" MODIFIED="1510402144211">
+<edge COLOR="#0000ff"/>
+<node TEXT="Trennung von Inhalt &amp; Layout" ID="ID_1134255793" CREATED="1510407167071" MODIFIED="1510407178408">
+<node TEXT="Inhalt in Posts &amp; Pages" ID="ID_1147722814" CREATED="1510407180745" MODIFIED="1510407200288"/>
+<node TEXT="Layout &#xfc;ber Vorlagen &amp; Themes" ID="ID_880177168" CREATED="1510407200728" MODIFIED="1510407210920"/>
+</node>
+<node TEXT="Templates" ID="ID_983272786" CREATED="1510407135538" MODIFIED="1510407138625">
+<node TEXT="https://codex.wordpress.org/Templates" ID="ID_468580708" CREATED="1510407138898" MODIFIED="1510407142465">
+<node TEXT="Bestimmen, wie Seite aussieht" ID="ID_1613869980" CREATED="1510407148218" MODIFIED="1510407155616"/>
+<node TEXT="Lesen Daten aus MySQL" ID="ID_834039270" CREATED="1510407255511" MODIFIED="1510407261784"/>
+<node TEXT="Generieren HTML" ID="ID_342480523" CREATED="1510407261943" MODIFIED="1510407266544"/>
+</node>
+<node TEXT="https://developer.wordpress.org/themes/basics/template-files/" ID="ID_1982192449" CREATED="1510407279502" MODIFIED="1510407280480">
+<node TEXT="PHP Dateien" ID="ID_798698301" CREATED="1510407295409" MODIFIED="1510407299295"/>
+<node TEXT="HTML Code" ID="ID_1800524679" CREATED="1510407299478" MODIFIED="1510407307752"/>
+<node TEXT="[https://developer.wordpress.org/themes/basics/template-tags/] Template Tags" ID="ID_1320186594" CREATED="1510407307958" MODIFIED="1510407425767">
+<node TEXT="Funktionen, um Inhalte aus DB auszulesen" ID="ID_868893576" CREATED="1510407430968" MODIFIED="1510407441327"/>
+<node TEXT="Parametrisierbar" ID="ID_733755903" CREATED="1510407454037" MODIFIED="1510407458798"/>
+</node>
+<node TEXT="PHP Code" ID="ID_1434659412" CREATED="1510407313406" MODIFIED="1510407319320"/>
+<node TEXT="Templates k&#xf6;nnen andere Templates enthalten" ID="ID_1802389277" CREATED="1510407491205" MODIFIED="1510407502238">
+<node TEXT="Wiederkehrende Elemente auslagern" ID="ID_1413171754" CREATED="1510407553095" MODIFIED="1510407564741"/>
+</node>
+</node>
+<node TEXT="https://developer.wordpress.org/themes/basics/template-hierarchy/" ID="ID_1350934961" CREATED="1510407640148" MODIFIED="1510407723725">
+<node TEXT="Anhand Query String wird entschieden, welches Template gerendert werden muss, um Seite anzuzeigen" ID="ID_1200726467" CREATED="1510407648550" MODIFIED="1510407698134"/>
+</node>
+</node>
+<node TEXT="[https://codex.wordpress.org/Using_Themes] Themes" ID="ID_1409520112" CREATED="1510407729476" MODIFIED="1510408098179">
+<node TEXT="Sammlung von verschiedenen Dateien" ID="ID_1816813625" CREATED="1510407823829" MODIFIED="1510407926460"/>
+<node TEXT="Bilden gemeinsam ein durchg&#xe4;ngiges Design f&#xfc;r den gesamten Blog" ID="ID_1304921253" CREATED="1510407838291" MODIFIED="1510407851236"/>
+<node TEXT="Enthalten" ID="ID_870300445" CREATED="1510407878259" MODIFIED="1510407881508">
+<node TEXT="Templates" ID="ID_422621797" CREATED="1510407887030" MODIFIED="1510407889124"/>
+<node TEXT="Bilder" ID="ID_296340309" CREATED="1510407889515" MODIFIED="1510407892156"/>
+<node TEXT="JS/CSS" ID="ID_1206659232" CREATED="1510407892315" MODIFIED="1510407897548"/>
+<node TEXT="PHP Code Dateien" ID="ID_1222693409" CREATED="1510407897755" MODIFIED="1510407902531"/>
+</node>
+<node TEXT="Muss nur die Dateien enthalten, die wirklich n&#xf6;tig sind. Muss nicht jedes Template &#xfc;berschreiben." ID="ID_538758459" CREATED="1510407963586" MODIFIED="1510408018235"/>
+</node>
+</node>
+<node TEXT="Plugins" POSITION="right" ID="ID_608155940" CREATED="1510411223787" MODIFIED="1510411225948">
+<edge COLOR="#7c007c"/>
+<node TEXT="Zur Erweiterung von Wordpress" ID="ID_51045353" CREATED="1510411226214" MODIFIED="1510411234149"/>
+<node TEXT="&#xd6;ffentlicher Marketplace" ID="ID_1223041635" CREATED="1510411234307" MODIFIED="1510411240236"/>
+<node TEXT="K&#xf6;nnen &#xfc;ber Admin Oberfl&#xe4;che installiert werden" ID="ID_1920640242" CREATED="1510411240387" MODIFIED="1510411252684"/>
+<node TEXT="Datenhaltung" ID="ID_233729852" CREATED="1510406460284" MODIFIED="1510411255661">
+<node TEXT="In DB wird HTML gespeichert" ID="ID_1340882985" CREATED="1510410670041" MODIFIED="1510410732936"/>
+<node TEXT="Plugins speichern eigenes Markup" ID="ID_266966839" CREATED="1510410733247" MODIFIED="1510410742368">
+<node TEXT="Z.B Form Maker Plugin: [Form id=&quot;1&quot;]" ID="ID_1360734039" CREATED="1510410742993" MODIFIED="1510410762416"/>
+<node TEXT="Markup wird zur Laufzeit zu HTML umgewandelt" ID="ID_1905483285" CREATED="1510410764247" MODIFIED="1510410788935"/>
+</node>
+</node>
+</node>
+<node TEXT="Auslieferung" POSITION="right" ID="ID_369893341" CREATED="1510401971441" MODIFIED="1510401977858">
+<edge COLOR="#00ff00"/>
+<node TEXT="Wordpress ist im Grunde eine PHP Anwendung" ID="ID_1489794322" CREATED="1510410971541" MODIFIED="1510410982902"/>
+<node TEXT="Code wird auf Anfrage ausgef&#xfc;hrt" ID="ID_1535482370" CREATED="1510410809402" MODIFIED="1510411000789"/>
+<node TEXT="Wordpress ist auch auslieferndes System" ID="ID_853118940" CREATED="1510411000949" MODIFIED="1510411011719"/>
+</node>
+<node TEXT="Allgemein" POSITION="left" ID="ID_1886282953" CREATED="1510403032945" MODIFIED="1510403036698">
+<edge COLOR="#00ffff"/>
+<node TEXT="https://wordpress.org/about/" ID="ID_539184905" CREATED="1510403198761" MODIFIED="1510403200249">
+<node TEXT="2003" OBJECT="java.lang.Long|2003" ID="ID_981144523" CREATED="1510403136205" MODIFIED="1510403138419"/>
+<node TEXT="Open Source" ID="ID_1842459802" CREATED="1510403150241" MODIFIED="1510403153626"/>
+<node TEXT="Als Bloggin System gestartet" ID="ID_883997006" CREATED="1510403183281" MODIFIED="1510403190393">
+<node TEXT="https://codex.wordpress.org/Introduction_to_Blogging" ID="ID_1282261665" CREATED="1510403911733" MODIFIED="1510404405208">
+<node TEXT="Zitat: &quot;Blog&quot; is an abbreviated version of &quot;weblog,&quot; which is a term used to describe websites that maintain an ongoing chronicle of information. A blog features diary-type commentary and links to articles on other websites, usually presented as a list of entries in reverse chronological order. Blogs range from the personal to the political, and can focus on one narrow subject or a whole range of subjects." ID="ID_170632676" CREATED="1510403924957" MODIFIED="1510404402417" HGAP_QUANTITY="20.749999798834327 pt" VSHIFT_QUANTITY="-20.24999939650297 pt"/>
+<node TEXT="Oft auch Privatpersonen" ID="ID_223485535" CREATED="1510404387863" MODIFIED="1510404405208" HGAP_QUANTITY="18.499999865889553 pt" VSHIFT_QUANTITY="-32.24999903887513 pt"/>
+<node TEXT="CM wird stark vereinfacht" ID="ID_688584620" CREATED="1510404362904" MODIFIED="1510404465266" VSHIFT_QUANTITY="-23.999999284744284 pt">
+<node TEXT="&quot;Zitat&quot;: Many blogging software programs are considered a specific type of CMS. They provide the features required to create and maintain a blog, and can make publishing on the internet as simple as writing an article, giving it a title, and organizing it under (one or more) categories." ID="ID_914691354" CREATED="1510404406913" MODIFIED="1510404465266" VSHIFT_QUANTITY="-36.74999890476469 pt"/>
+</node>
+</node>
+</node>
+<node TEXT="Jetzt volles CMS" ID="ID_230078462" CREATED="1510403217233" MODIFIED="1510403344354">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1321607354" MIDDLE_LABEL="durch" STARTINCLINATION="94;0;" ENDINCLINATION="94;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+<node TEXT="Community Plugins &amp; Themes" ID="ID_1321607354" CREATED="1510403243449" MODIFIED="1510403261025">
+<node TEXT="https://codex.wordpress.org/Plugins" ID="ID_212736802" CREATED="1510408231555" MODIFIED="1510408232906"/>
+</node>
+<node TEXT="Nutzt PHP &amp; MySQL" ID="ID_983547982" CREATED="1510403278345" MODIFIED="1510403284657"/>
+</node>
+</node>
+<node TEXT="Posts und Pages" POSITION="left" ID="ID_1805574387" CREATED="1510401864619" MODIFIED="1510401872097">
+<edge COLOR="#ff0000"/>
+<node TEXT="https://codex.wordpress.org/Pages" ID="ID_1855722176" CREATED="1510406153182" MODIFIED="1510406155727">
+<node TEXT="In WordPress, you can put content on your site as either a &quot;post&quot; or a &quot;page&quot;." ID="ID_1228726825" CREATED="1510406141831" MODIFIED="1510406142976"/>
+</node>
+<node TEXT="[https://codex.wordpress.org/Posts_Add_New_SubPanel] Posts" ID="ID_984440002" CREATED="1510404742626" MODIFIED="1510405685730">
+<node TEXT="&quot;Posts are entries that display in reverse order on your home page.&quot;" ID="ID_1061212236" CREATED="1510406209302" MODIFIED="1510406214260"/>
+<node TEXT="Zentrale Datenstruktur f&#xfc;r Inhalte, wegen Blog-Vergangenheit" ID="ID_819172985" CREATED="1510404692181" MODIFIED="1510404737003"/>
+<node TEXT="Felder" ID="ID_1549711520" CREATED="1510404767314" MODIFIED="1510404769939">
+<node TEXT="Titel" ID="ID_1367908406" CREATED="1510404770445" MODIFIED="1510404773339"/>
+<node TEXT="Body Content" ID="ID_1140341471" CREATED="1510404773698" MODIFIED="1510404803986"/>
+</node>
+<node TEXT="Weitere Optionen" ID="ID_1356790569" CREATED="1510405171358" MODIFIED="1510405175222">
+<node TEXT="Kann verschiedenen Kategorien zugewiesen werden: https://codex.wordpress.org/Posts_Categories_SubPanel" ID="ID_369249467" CREATED="1510404819921" MODIFIED="1510405711747"/>
+<node TEXT="Kann Tags erhalten: https://codex.wordpress.org/Posts_Tags_SubPanel" ID="ID_1690755410" CREATED="1510405005351" MODIFIED="1510405719186"/>
+<node TEXT="Excerpt" ID="ID_116314598" CREATED="1510405267949" MODIFIED="1510405269301"/>
+<node TEXT="Sichtbarkeit" ID="ID_1230301080" CREATED="1510405181581" MODIFIED="1510405207902">
+<node TEXT="&#xd6;ffentlich" ID="ID_1085586444" CREATED="1510405209327" MODIFIED="1510405212846"/>
+<node TEXT="Passwortgesch&#xfc;tzt" ID="ID_1146011839" CREATED="1510405213229" MODIFIED="1510405218542"/>
+<node TEXT="Privat" ID="ID_99148136" CREATED="1510405218741" MODIFIED="1510405220550"/>
+</node>
+<node TEXT="Custom Fields" ID="ID_1377248076" CREATED="1510405278765" MODIFIED="1510405282488">
+<node TEXT="Auch: https://codex.wordpress.org/Custom_Fields" ID="ID_707300638" CREATED="1510405631697" MODIFIED="1510405634474"/>
+<node TEXT="Zus&#xe4;tzliche Infos an Post speichern" ID="ID_893268060" CREATED="1510405285926" MODIFIED="1510405345381"/>
+<node TEXT="Von Plugins genutzt" ID="ID_1811811320" CREATED="1510405351244" MODIFIED="1510405356900"/>
+<node TEXT="F&#xfc;r Metadaten gedacht" ID="ID_64162576" CREATED="1510405358267" MODIFIED="1510405550658"/>
+<node TEXT="Werden nicht genutzt, um Inhalte zu strukturieren" ID="ID_11998" CREATED="1510405608505" MODIFIED="1510405620898"/>
+<node TEXT="Set der verf&#xfc;gbaren Metadaten f&#xfc;r alle Posts identisch. Deshalb nicht sinnvoll geeignet f&#xfc;r Seitentypen." ID="ID_246680186" CREATED="1510405550842" MODIFIED="1510405596554"/>
+</node>
+</node>
+<node TEXT="[https://codex.wordpress.org/Post_Types] Post Types" ID="ID_1284934260" CREATED="1510405902960" MODIFIED="1510405939849">
+<node TEXT="Alle Posts werden in einer Tabelle gespeichert" ID="ID_936819435" CREATED="1510405953449" MODIFIED="1510405964456"/>
+<node TEXT="Posts k&#xf6;nnen spezielle Typen haben" ID="ID_1186217881" CREATED="1510405964727" MODIFIED="1510405979808"/>
+<node TEXT="Standard-Typen" ID="ID_611588374" CREATED="1510405980847" MODIFIED="1510405986648">
+<node TEXT="Post" ID="ID_1050057856" CREATED="1510405992266" MODIFIED="1510405993144"/>
+<node TEXT="Page" ID="ID_1165688511" CREATED="1510405993408" MODIFIED="1510405997632"/>
+<node TEXT="Attachment" ID="ID_1273383823" CREATED="1510405997896" MODIFIED="1510406013617"/>
+<node TEXT="Revision" ID="ID_1552564168" CREATED="1510406017215" MODIFIED="1510406018032"/>
+<node TEXT="Navigation Menu" ID="ID_140712623" CREATED="1510406018199" MODIFIED="1510406025016"/>
+<node TEXT="Custom CSS" ID="ID_132062049" CREATED="1510406025191" MODIFIED="1510406030168"/>
+<node TEXT="Changesets" ID="ID_297893280" CREATED="1510406030311" MODIFIED="1510406036448"/>
+</node>
+<node TEXT="Eigene Post Types m&#xf6;glich" ID="ID_1675921530" CREATED="1510406037703" MODIFIED="1510406043360"/>
+<node TEXT="&#xc4;ndert nichts an den Eingabekomponenten, da DB-Tabelle auch identisch" ID="ID_1098640919" CREATED="1510406062679" MODIFIED="1510406080368"/>
+</node>
+</node>
+<node TEXT="[https://codex.wordpress.org/Pages] Pages" ID="ID_243892590" CREATED="1510405687785" MODIFIED="1510406177480">
+<node TEXT="Post mit dem Post Type &quot;Page&quot;" ID="ID_1314624541" CREATED="1510406113585" MODIFIED="1510406120624"/>
+<node TEXT="&quot;In contrast, pages are generally for non-chronological, hierarchical content: pages like &quot;About&quot; or &quot;Contact&quot; would be common examples. See below for examples of subpages in a hierarchy. Pages live outside of the normal blog chronology, and are often used to present timeless information about yourself or your site -- information that is always relevant. You can use Pages to organize and manage the structure of your website content.&quot;" ID="ID_158440756" CREATED="1510406294158" MODIFIED="1510406298055"/>
+<node TEXT="Haben ebenfalls Titel und Inhalt" ID="ID_997783119" CREATED="1510406331941" MODIFIED="1510406360374"/>
+<node TEXT="Keine Kategorien" ID="ID_525770781" CREATED="1510406360533" MODIFIED="1510406364510"/>
+<node TEXT="Haben Tags" ID="ID_1444049987" CREATED="1510406368021" MODIFIED="1510406371550"/>
+</node>
+</node>
+</node>
+</map>
