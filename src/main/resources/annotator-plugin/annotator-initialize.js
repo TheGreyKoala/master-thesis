e9ace77985401882cff812ace7acf174f87081d6
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-12-28 16:42:57 +0100

    Preformulare annotator plugin

---
+++ src/main/resources/annotator-plugin/annotator-initialize.js
@@ -0,0 +1,12 @@
+jQuery(function ($) {
+    const annotator = $(document.body).annotator().data("annotator");
+    annotator
+        .addPlugin("Store", {
+            prefix: "http://<ANNOTATION_SERVICE_HOST>:16612/pages/" + encodeURIComponent("<PAGE_URL>")
+        })
+        .addPlugin("wccs")
+        .addPlugin("Permissions", {
+            user: "editor",
+            permissions: { "admin": ["wccs"] }
+        });
+});
