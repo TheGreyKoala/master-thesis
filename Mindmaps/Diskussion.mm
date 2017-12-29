97becba10bdbb684fd50b265410f41d76da299ce
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-12-29 19:04:28 +0100

    Working on findings

---
+++ Mindmaps/Diskussion.mm
@@ -0,0 +1,95 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Diskussion" FOLDED="false" ID="ID_1129469300" CREATED="1514538636418" MODIFIED="1514538642122" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Abdeckung der Dom&#xe4;ne" POSITION="right" ID="ID_1332383261" CREATED="1514538648248" MODIFIED="1514538657080">
+<edge COLOR="#ff0000"/>
+<node TEXT="Wiederholende Elemente ohne umschlie&#xdf;enden Tag schwer" ID="ID_722876685" CREATED="1514538657913" MODIFIED="1514538681663"/>
+</node>
+<node TEXT="Ansatz" POSITION="right" ID="ID_755003799" CREATED="1514538682639" MODIFIED="1514538684720">
+<edge COLOR="#0000ff"/>
+<node TEXT="Klassen m&#xfc;ssen iterativ entwickelt werden" ID="ID_1579508934" CREATED="1514538685265" MODIFIED="1514538696047"/>
+</node>
+<node TEXT="Vererbungskonzept w&#xe4;re sinnvoll, Definition f&#xfc;r allgemeine Bereiche &#xfc;berall gleich" POSITION="right" ID="ID_1065252782" CREATED="1514540409708" MODIFIED="1514540449350">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Fehlende Syntax Checks machen Schreiben von CSS und XPath schwer und fehleranf&#xe4;llig. Dazu wird drittes Tool verwendet." POSITION="right" ID="ID_1033591712" CREATED="1514544546253" MODIFIED="1514544615121">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Christopher Dorn (PVS)" POSITION="left" ID="ID_273042609" CREATED="1514547369251" MODIFIED="1514569368125">
+<edge COLOR="#7c0000"/>
+<node TEXT="Wurde 2x klassifiziert" ID="ID_1643551589" CREATED="1514547384826" MODIFIED="1514547390993"/>
+<node TEXT="Weil ung&#xfc;nstige Konstellation aus CSS Selektor und Node Struktur" ID="ID_1573582082" CREATED="1514547391208" MODIFIED="1514547406161">
+<node TEXT="team-member-des geschachtelt" ID="ID_1776108256" CREATED="1514547406410" MODIFIED="1514547415770"/>
+</node>
+<node TEXT="D.h. man muss  genau gucken" ID="ID_904370754" CREATED="1514547417696" MODIFIED="1514547425937"/>
+<node TEXT="Gleiches gilt auch f&#xfc;r andere (siehe info.txt)" ID="ID_54617364" CREATED="1514569374938" MODIFIED="1514569385675"/>
+</node>
+<node TEXT="Katharina Bengin (PVS)" POSITION="left" ID="ID_994630311" CREATED="1514569340003" MODIFIED="1514569372315">
+<edge COLOR="#00007c"/>
+<node TEXT="&#xdc;berschrift wurde f&#xe4;lschlicherweise als Name erkannt" ID="ID_1589153216" CREATED="1514569346421" MODIFIED="1514569361331"/>
+</node>
+</node>
+</map>
---
+++ Mindmaps/Ergebnisse.mm
@@ -0,0 +1,91 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Ergebnisse" FOLDED="false" ID="ID_1470724918" CREATED="1514538143736" MODIFIED="1514538148071" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Vorgehen" POSITION="right" ID="ID_919487286" CREATED="1514538173149" MODIFIED="1514538175605">
+<edge COLOR="#ff0000"/>
+<node TEXT="2 Fallbeispiele" ID="ID_1994669704" CREATED="1514538175854" MODIFIED="1514538181812">
+<node TEXT="Kontakte" ID="ID_990776361" CREATED="1514538182061" MODIFIED="1514538185741"/>
+<node TEXT="News-&#xdc;bersicht" ID="ID_642862334" CREATED="1514538185898" MODIFIED="1514538191004"/>
+</node>
+<node TEXT="In der ersten" ID="ID_1264769444" CREATED="1514538203379" MODIFIED="1514538226892">
+<node TEXT="Klassen f&#xfc;r komplette Seite schreiben" ID="ID_1323908827" CREATED="1514538227365" MODIFIED="1514538278717">
+<node TEXT="Header, Footer etc." ID="ID_1251627988" CREATED="1514538250180" MODIFIED="1514538261516"/>
+<node TEXT="Eigentlicher Inhalt" ID="ID_1715619846" CREATED="1514538261858" MODIFIED="1514538272011"/>
+</node>
+<node TEXT="Konfig auf alle Sites loslassen" ID="ID_554747209" CREATED="1514538313682" MODIFIED="1514538331795"/>
+</node>
+<node TEXT="In der zweiten" ID="ID_300119423" CREATED="1514538282650" MODIFIED="1514538285867">
+<node TEXT="Klassen f&#xfc;r allgemeine Inhalte wiederverwenden aus 1. Fallbeispiel" ID="ID_669879323" CREATED="1514538287742" MODIFIED="1514538311051"/>
+<node TEXT="Klassen f&#xfc;r eigentlichen Inhalt schreiben" ID="ID_1446859688" CREATED="1514538391081" MODIFIED="1514538406579"/>
+<node TEXT="Konfig auf alle Sites anwenden" ID="ID_519871764" CREATED="1514538406745" MODIFIED="1514538413139"/>
+</node>
+</node>
+<node TEXT="Ergebnis-Artefakte" POSITION="right" ID="ID_1875592408" CREATED="1514538581623" MODIFIED="1514538598016">
+<edge COLOR="#0000ff"/>
+<node TEXT="Programme" ID="ID_262286958" CREATED="1514538599042" MODIFIED="1514538605249"/>
+</node>
+</node>
+</map>
