18349ff7974f591e7a0c50b446b1e92f1e226395
Tim Gremplewski <tim.gremplewski@gmail.com>
2018-11-03 17:29:16 +0100

    Removed files of other course

--- Literatur/01888_Domänenspezifisch-Sprachen/Lösungen/EA1/Gremplewski_Tim_9514244_1888/EA1/Prolog/Aufgabe1.pl
+++
@@ -1,6 +0,0 @@
-addiere(0, Y, Y) :- valid(Y).
-addiere(succ(X), Y, Z) :- addiere(X, succ(Y), Z).
-
-% Ensure that only terms like '0', 'succ(0)', 'succ(succ(0))' etc. are accepted
-valid(0).
-valid(succ(Q)) :- valid(Q).
